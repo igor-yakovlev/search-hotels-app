bc30c49740d68e68807e1c5a810fad2a8e3dca93
Ivan Osipov <ivan.osipov@solveva.com>
2022-04-15 23:49:51 +0300

    master/fetch-data

--- src/component/Forms/SearchForm/SearchForm.jsx
+++ src/component/Forms/SearchForm/SearchForm.jsx
@@ -1,101 +1,64 @@
-import React, { useCallback, useState } from 'react';
+import React, {useState} from 'react';
 import Button from '../../Buttons/Button';
 import Input from '../../Input/Input';
 import styles from './SearchForm.module.css'
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
+const SearchForm = ({defaultValue = {}, onSubmit}) => {
+  const [form, setForm] = useState(defaultValue);
 
-// location=Moscow&currency=rub&checkIn=2022-12-10&checkOut=2022-12-12&limit=10
-
-const initialSearch = {
-  location: 'Москва',
-  currency: 'rub',
-  checkIn: new Date().toISOString().slice('0', '10'),
-  checkOut: '',
-}
-
-const API__HOTELS = 'api/v2/cache.json';
-
-const SearchForm = () => {
-  const [search, setSearch] = useState(initialSearch);
-  const [datePicker, setDatePicker] = useState(new Date());
-  const [numberOfDays, setnumberOfDays] = useState(1);
-
-  const handleChange = useCallback(({target}) => {
+  const handleChange = ({target}) => {
     const {name, value} = target;
-    setSearch(state => ({...state, [name]: value}))
-  }, [setSearch]);
+    setForm(state => ({...state, [name]: value}))
+  };
 
-  const handledatePickerChange = useCallback((e) => {
-    const date = e;
-    console.log(e.toISOString())
-    const checkInDate = date.toISOString().slice('0', '10');
-    setDatePicker(date);
-    setSearch(state => ({...state, checkIn: checkInDate}))
-  } , [setSearch, setDatePicker]);
-
-
-  const handlenumberOfDays = useCallback(({target}) => {
-    const {value} = target;
-    const checkInDate = datePicker;
-    const checkOutDate = checkInDate.setDate(checkInDate.getDate() + +value);
-    console.log(new Date(checkOutDate));
-    setSearch(state => ({...state, checkOut: new Date(checkOutDate).toISOString().slice('0', '10')}))
-    setnumberOfDays(value);
-  } , [datePicker, setnumberOfDays]);
-
-
-  const handleSubmit = useCallback((e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const url = new URL(`${window.location.origin}/${API__HOTELS}`);
-    // Object.entries(search).forEach(param => url.searchParams.append(...param))
-    // console.log(new Date(search.checkIn).getTime());
-    console.log(search);
-    console.log(Object.entries(search).forEach(item => url.searchParams.append(...item)));
+    onSubmit(form);
+  };
 
-  }, [search]);
-
-    return (
-      <div className={styles.wrapper}>
-            <form onSubmit={handleSubmit} className={styles.form}>
-              <div className={styles.form__body}>
-                <label className={styles.input__label}>
-                    Локация
-                    <Input
-                    onChange={handleChange}
-                    value={search.location}
-                    required
-                    name={"location"}
-                    type="text"
-                    />
-                </label>
-                <label className={styles.input__label}>
-                    Дата заселения
-                    <DatePicker
-                      dateFormat="dd.MM.yyyy"
-                      className={styles.date}
-                      selected={datePicker}
-                      onChange={handledatePickerChange}
-                    />
-                </label>
-                <label className={styles.input__label}>
-                    Количество дней
-                    <Input
-                    value={numberOfDays}
-                    onChange={handlenumberOfDays}
-                    required
-                    name={"numOfDays"}
-                    type="text"
-                    />
-                </label>
-              </div>
-              <div className={styles.form__control}>
-                <Button type={'submit'}>Найти</Button>
-              </div>
-            </form>
+  return (
+    <div className={styles.wrapper}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.form__body}>
+          <label className={styles.input__label}>
+            Локация
+            <Input
+              onChange={handleChange}
+              value={form.location}
+              required
+              name={"location"}
+              type="text"
+            />
+          </label>
+          <label className={styles.input__label}>
+            Дата заселения
+            <DatePicker
+              dateFormat="dd.MM.yyyy"
+              className={styles.date}
+              selected={form.checkIn}
+              name={'checkIn'}
+              onChange={value => handleChange({target: {name: 'checkIn', value}})}
+            />
+          </label>
+          <label className={styles.input__label}>
+            Количество дней
+            <Input
+              value={form.daysCount}
+              onChange={handleChange}
+              required
+              name={"daysCount"}
+              type="number"
+            />
+          </label>
         </div>
-    );
+        <div className={styles.form__control}>
+          <Button type={'submit'}>Найти</Button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SearchForm;
--- src/screens/MainScreen/MainScreen.jsx
+++ src/screens/MainScreen/MainScreen.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import FavoritePanel from '../../component/FavoritePanel/FavoritePanel';
 import SearchForm from '../../component/Forms/SearchForm/SearchForm';
 import MainPanel from '../../component/MainPanel/MainPanel';
-import styles  from './MainScreen.module.css';
+import styles from './MainScreen.module.css';
 import {useNavigate} from "react-router-dom";
+import {format} from "date-fns";
+import {useFetchHotels} from "../../component/Forms/SearchForm/useFetchHotels";
+
+
+const DEFAULT_DAYS_COUNT = 1;
+// location=Moscow&currency=rub&checkIn=2022-12-10&checkOut=2022-12-12&limit=10
+const initialParams = {
+  location: 'Москва',
+  currency: 'rub',
+  checkIn: format(new Date(), 'yyyy-MM-dd'),
+  checkOut: format(new Date().setDate(new Date().getDate() + DEFAULT_DAYS_COUNT), 'yyyy-MM-dd'),
+  limit: 10
+};
+
 
 const MainScreen = () => {
-
   const navigate = useNavigate();
+  const [params, setParams] = useState(initialParams);
+  const data = useFetchHotels(params)
+
+  console.log({data});
+
   const handleClick = () => {
     navigate('/')
+  }
+
+  const handleSubmit = (e) => {
+    const {location, checkIn, daysCount} = e;
+    const checkOut = new Date(checkIn);
+    checkOut.setDate(checkIn.getDate() + Number(daysCount));
+
+    setParams(state => ({
+      ...state,
+      location,
+      checkIn: format(checkIn, 'yyyy-MM-dd'),
+      checkOut: format(checkOut, 'yyyy-MM-dd')
+    }))
   }
 
   return (
@@ -20,7 +51,11 @@ const MainScreen = () => {
           <button onClick={handleClick} className={styles.main__exit_button}>Выйти</button>
         </header>
         <section className={styles.main__content}>
-          <SearchForm className={styles.searchForm}/>
+          <SearchForm className={styles.searchForm} defaultValue={{
+            location: initialParams.location,
+            checkIn: new Date(initialParams.checkIn),
+            daysCount: DEFAULT_DAYS_COUNT
+          }} onSubmit={handleSubmit}/>
           <FavoritePanel className={styles.favoritePanel}/>
           <MainPanel className={styles.mainPanel}/>
         </section>
